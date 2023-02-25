961badfa0e3f134740f6895cd8648f298ac3ecd6
Amirah <fitriamirah99@gmail.com>
2023-02-25 10:34:21 +0800

    after successfully pushing, now adding new line, then add/stage/track, then commit

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log("testing for git command line new branch, add new code, stage, commit and push");
+console.log("this is after successfully pushing to remote repo. now trying to add new code, and then add/stage/track, and then commit");
